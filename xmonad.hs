6c2c846961f28a7bfbe5e7f6e0860dc55f5a574e
Peter Tseng <pht24@cornell.edu>
2016-12-10 13:35:40 -0800

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,58 @@
+import XMonad
+import XMonad.Actions.CopyWindow
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
+import System.Environment (getEnv)
+import System.IO
+
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+-- DMenu settings...
+selected   = "'#0000cc'"
+background = "'#cccccc'"
+foreground = "'#000000'"
+
+myDmenuTitleBar =
+    "exec `dmenu_run \
+        \ -p 'Run:' -i \
+        \ -nb " ++ background ++ "\
+        \ -nf " ++ foreground ++ "\
+        \ -sb " ++ selected   ++ "\
+    \`"
+
+main = do
+    home <- getEnv "HOME"
+    xmproc <- spawnPipe $ "/usr/bin/xmobar " ++ home ++"/.xmonad/xmobar.hs"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 100
+            }
+        , modMask = mod4Mask
+        , terminal = "urxvt"
+        , keys = newKeys
+        }
+
+myKeys x@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
+    , ((modm,               xK_Return), spawn $ XMonad.terminal x)
+    , ((modm,               xK_e     ), spawn "urxvt -e ranger")
+    , ((modm,               xK_p     ), spawn myDmenuTitleBar)
+    ]
+    ++
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    -- mod-control-[1..9], Copy client to workspace N
+    --
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces x) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask), (copy, controlMask)]
+    ]
+
+newKeys x = M.union (myKeys x) (keys defaultConfig x)
