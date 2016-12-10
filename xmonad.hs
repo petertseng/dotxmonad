28e6dee544158e2d9d0120841045db7f2b3bad5c
Peter Tseng <pht24@cornell.edu>
2016-12-10 13:56:18 -0800

    pass $HOME on the command line
    spawnPipe interprets this right, so I don't need to use getEnv. Since
    getEnv fails with an error, the impact of not having HOME has been
    lessened from "xmonad won't start" to "xmonad starts without xmobar"
    which seems far superior.

--- xmonad.hs
+++ xmonad.hs
@@ -12,7 +12,6 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
-import System.Environment (getEnv)
 import System.IO (hPutStrLn)
 
 import qualified Data.Map as M
@@ -32,8 +31,7 @@ myDmenuTitleBar =
     \`"
 
 main = do
-    home <- getEnv "HOME"
-    xmproc <- spawnPipe $ "/usr/bin/xmobar " ++ home ++"/.xmonad/xmobar.hs"
+    xmproc <- spawnPipe "/usr/bin/xmobar $HOME/.xmonad/xmobar.hs"
     xmonad $ def
         { manageHook = manageDocks <+> manageHook def
         , layoutHook = avoidStruts  $  layoutHook def
