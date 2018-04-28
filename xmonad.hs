8a2e43c66004b69d61c9b9b21feb1b81e0339da0
Peter Tseng <pht24@cornell.edu>
2018-04-28 07:21:39 +0000

    add extended window manager hints
    an interesting behaviour: Letting apps query the window list

--- xmonad.hs
+++ xmonad.hs
@@ -9,6 +9,7 @@ import XMonad.Hooks.DynamicLog
   , xmobarColor
   , xmobarPP
   )
+import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks)
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
@@ -32,7 +33,7 @@ myDmenuTitleBar =
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar $HOME/.xmonad/xmobar.hs"
-    xmonad $ def
+    xmonad $ emwh def
         { modMask = mod4Mask
         , terminal = "urxvt"
         , keys = newKeys
